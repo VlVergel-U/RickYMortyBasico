4c66301d903a39b1f1622150b3bfc7d6d6321e04
Valentina Vergel <vvergelv@ufpso.edu.co>
2024-11-11 13:21:42 -0500

    Actualizado

---
+++ Parcial2/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import path from 'path';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'login', pathMatch:'full'
+    },
+    {
+        path: 'login',
+        component:LoginComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children:[
+            {
+                path: 'dashborad',
+                component: DashboardComponent
+            }
+        ]
+    }
+];
---
+++ Parcial2/src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
